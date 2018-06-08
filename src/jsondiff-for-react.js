a93250989f0bb52a5f1c317c58299ba367c7456a
Arek <arek@yaska.eu>
2018-06-08 16:21:41 +0200

    updated jsondiffpatch

--- src/jsondiff-for-react.js
+++ src/jsondiff-for-react.js
@@ -3,11 +3,10 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import jsondiffpatch from 'jsondiffpatch/src/main';
-import formatters from 'jsondiffpatch/src/main-formatters';
+import { diff, formatters } from 'jsondiffpatch/dist/jsondiffpatch.umd.js';
 
-require('jsondiffpatch/public/formatters-styles/html.css');
-require('jsondiffpatch/public/formatters-styles/annotated.css');
+require('jsondiffpatch/dist/formatters-styles/html.css');
+require('jsondiffpatch/dist/formatters-styles/annotated.css');
 
 class JsonDiffReact extends Component {
   static propTypes = {
@@ -18,17 +17,18 @@ class JsonDiffReact extends Component {
     tips: PropTypes.string,
   };
   render() {
-    const {right, left, show = true, annotated = false, tips} = this.props;
-    const delta = jsondiffpatch.diff(left, right);
-    const html = annotated ?
-      formatters.annotated.format(delta) : formatters.html.format(delta, left);
+    const { right, left, show = true, annotated = false, tips } = this.props;
+    const delta = diff(left, right);
+    const html = annotated
+      ? formatters.annotated.format(delta)
+      : formatters.html.format(delta, left);
     show ? formatters.html.showUnchanged() : formatters.html.hideUnchanged();
     const text = tips || '无差异！';
-    return (
-      html ?
-        <div dangerouslySetInnerHTML={{__html: html}} /> :
-        <p style={{fontSize: 12, color: '#999'}}>{text}</p>
-    );
+    return html ? (
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    ) : (
+        <p style={{ fontSize: 12, color: '#999' }}>{text}</p>
+      );
   }
 }
 export default JsonDiffReact;
